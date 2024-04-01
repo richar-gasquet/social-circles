c51d73dbb6592c9605612094f2472b5cf20f37c7
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-01 16:07:40 -0400

    Prettify format

--- social-circles/frontend/src/pages/headers/GuestHeader.jsx
+++ social-circles/frontend/src/pages/headers/GuestHeader.jsx
@@ -1,33 +1,41 @@
-import { NavLink } from 'react-router-dom'
-import logo from '../../assets/social-circles-logo.png';
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/social-circles-logo.png";
 
 function GuestHeader() {
-    return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <NavLink to="/" className="navbar-brand">
-                    <img src={logo} alt="Logo" className="img-fluid"/>
-                </NavLink>
-                <h2>Social Circles</h2>
-                <div className="collapse navbar-collapse">
-                    <ul className = "navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <NavLink to="/about" className="nav-link">About Us</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/contact" className="nav-link">Contact</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/resources" className="nav-link">Resources</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/login" className="nav-link">Log In</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink to="/" className="navbar-brand">
+          <img src={logo} alt="Logo" className="img-fluid" />
+        </NavLink>
+        <h2>Social Circles</h2>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/contact" className="nav-link">
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/resources" className="nav-link">
+                Resources
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/login" className="nav-link">
+                Log In
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default GuestHeader
+export default GuestHeader;
--- social-circles/frontend/src/pages/user/EventCard.jsx
+++ social-circles/frontend/src/pages/user/EventCard.jsx
@@ -1,30 +1,30 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 function EventCard(props) {
-    return(
-        <div className="event-card">
-            <img className="event-image" src="https://via.placeholder.com/200"></img>
-            <h2 className="event-title">{props.name}</h2>
-            <p className="event-text">Date and Time: {props.date_and_time} </p>
-            <p className="event-text">
-                {props.filled_spots} out of {props.capacity} have already registered
-            </p>
-        </div>
-    );
+  return (
+    <div className="event-card">
+      <img className="event-image" src="https://via.placeholder.com/200"></img>
+      <h2 className="event-title">{props.name}</h2>
+      <p className="event-text">Date and Time: {props.date_and_time} </p>
+      <p className="event-text">
+        {props.filled_spots} out of {props.capacity} have already registered
+      </p>
+    </div>
+  );
 }
 
 EventCard.propTypes = {
-    name: PropTypes.string,
-    date_and_time: PropTypes.string,
-    capacity: PropTypes.number,
-    filled_spots: PropTypes.number,
-}
+  name: PropTypes.string,
+  date_and_time: PropTypes.string,
+  capacity: PropTypes.number,
+  filled_spots: PropTypes.number,
+};
 
 EventCard.defaultProps = {
-    name: "No title",
-    date_and_time: "N/A",
-    capacity: "N/A",
-    filled_spots: "N/A",
-}
+  name: "No title",
+  date_and_time: "N/A",
+  capacity: "N/A",
+  filled_spots: "N/A",
+};
 
-export default EventCard
+export default EventCard;
