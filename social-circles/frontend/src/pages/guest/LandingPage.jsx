e860115a21f7da4925327745eea8bfbef1a1e7ca
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-01 17:54:00 -0400

    Prettify LandingPage.jsx

--- social-circles/frontend/src/pages/guest/LandingPage.jsx
+++ social-circles/frontend/src/pages/guest/LandingPage.jsx
@@ -1,14 +1,18 @@
-import { NavLink } from 'react-router-dom'
-import GuestHeader from '../headers/GuestHeader'
+import { NavLink } from "react-router-dom";
+import GuestHeader from "../headers/GuestHeader";
 
 function LandingPage() {
-    return (
-        <>
-            <GuestHeader />
-            <p><NavLink to ='/user-dashboard'>Access User Dashboard</NavLink></p>
-            <p><NavLink to = '/login'>Click here to go to login page.</NavLink></p>
-        </>
-    )
+  return (
+    <>
+      <GuestHeader />
+      <p>
+        <NavLink to="/user-dashboard">Access User Dashboard</NavLink>
+      </p>
+      <p>
+        <NavLink to="/login">Click here to go to login page.</NavLink>
+      </p>
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
