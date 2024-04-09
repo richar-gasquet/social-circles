c655e7c29ca9ae92380ef04d13f5c5a65cf714fc
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-09 01:22:04 -0400

    Create an aside containing relevant links for events page.

--- social-circles/frontend/src/pages/user/Events.jsx
+++ social-circles/frontend/src/pages/user/Events.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import UserHeader from "../headers/UserHeader";
+import EventsAside from "./EventsAside.jsx";
 import EventCard from "./EventCard.jsx";
 
 function Events() {
@@ -32,32 +33,34 @@ function Events() {
   return (
     <>
       <UserHeader />
-      <h2>This is the Events page.</h2>
-      <p>Here are all available events: </p>
-      <div className="container">
-        <div className="row">
-          {events.map((event) => (
-            <div key={event.event_id} className="col-lg-4 col-md-6 col-sm-12">
-              <EventCard
-                name = {event.name}
-                desc = {event.desc}
-                start = {event.start_time}
-                end = {event.end_time}
-                capacity = {event.capacity}
-                filled = {event.filled_spots}
-                image = {event.image}>
-              </EventCard>
+      <div className={`container mt-3`}>
+        <div className={`row`}>
+          <h1 className={`ml-4`}>Upcoming Events</h1>
+        </div>
+        <div className={`row`}>
+          <EventsAside />
+          <div className={`col-lg-9 mt-3`}>
+            <div className={`row`}>
+              {events.map((event) => (
+                <div key={event.event_id} className={`col-lg-4 col-md-6 col-sm-12`}>
+                  <EventCard
+                    name={event.name}
+                    desc={event.desc}
+                    start={event.start_time}
+                    end={event.end_time}
+                    capacity={event.capacity}
+                    filled={event.filled_spots}
+                    image={event.image}>
+                  </EventCard>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>  
       </div>
-        
-
-      <NavLink to="/registered-events">
-        Click here to see the events you're registered for{" "}
-      </NavLink>
     </>
   );
 }
+
 
 export default Events;
---
+++ social-circles/frontend/src/pages/user/EventsAside.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+import styles from "./EventsAside.module.css"
+
+function EventsAside() {
+    return (
+        <>
+          <div className={`col-lg-3 mb-3`}>
+            <aside className={`mt-3`}>
+              <ul className={`nav flex-column text-muted`}>
+                <li className={`nav-item`}>
+                  <NavLink className={({ isActive }) => `nav-link ${styles.navLink} ${isActive ? styles.activeNavLink : ''}`} 
+                    to="/events">Upcoming Events</NavLink>
+                </li>
+                <li className={`nav-item`}>
+                  <NavLink className={({ isActive }) => `nav-link ${styles.navLink} ${isActive ? styles.activeNavLink : ''}`} 
+                    to="/past-events">Past Events</NavLink>
+                </li>
+                <li className={`nav-item`}>
+                  <NavLink className={({ isActive }) => `nav-link ${styles.navLink} ${isActive ? styles.activeNavLink : ''}`} 
+                    to="/registered-events">Registered Events</NavLink>
+                </li>
+                <li className={`nav-item`}>
+                  <NavLink className={({ isActive }) => `nav-link ${styles.navLink} ${isActive ? styles.activeNavLink : ''}`} 
+                    to="/events-with-dana">Events with Dana</NavLink>
+                </li>
+              </ul>
+            </aside>
+          </div>
+        </>
+    );
+}
+
+export default EventsAside;
