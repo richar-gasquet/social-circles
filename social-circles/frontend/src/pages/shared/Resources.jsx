6f0824ce6741a8653118a68ae4d852a59f7ba6e9
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-08 23:32:59 -0400

    Add conditional rendering for header

--- social-circles/frontend/src/pages/shared/Resources.jsx
+++ social-circles/frontend/src/pages/shared/Resources.jsx
@@ -1,9 +1,19 @@
 import GuestHeader from "../headers/GuestHeader";
+import UserHeader from "../headers/UserHeader";
+import { useAuthContext } from "../auth/AuthHandler";
 
 function Resources() {
+  const { isAuth } = useAuthContext();
+
+  let header = <GuestHeader />
+
+  if (isAuth) {
+    header = <UserHeader />
+  }
+
   return (
     <>
-      <GuestHeader />
+      {header}
     </>
   );
 }
