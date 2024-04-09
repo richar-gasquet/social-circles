3e72c45579917e07a44734ed401bb444255224f3
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-09 11:48:36 -0400

    Add communities pages

---
+++ social-circles/frontend/src/pages/user/CommunitiesAside.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Aside.module.css"
+
+function CommunitiesAside() {
+    return (
+        <>
+          <div className={`col-lg-2 mb-3 border-right`}>
+            <aside className={`mt-3`}>
+              <ul className={`nav flex-column`}>
+                <li className={`nav-item`}>
+                  <NavLink className={({ isActive }) => `nav-link ${styles.navLink} ${isActive ? styles.activeNavLink : ''}`} 
+                    to="/communities">All Communities</NavLink>
+                </li>
+                <li className={`nav-item`}>
+                  <NavLink className={({ isActive }) => `nav-link ${styles.navLink} ${isActive ? styles.activeNavLink : ''}`} 
+                    to="/my-communities">My Communities</NavLink>
+                </li>
+              </ul>
+            </aside>
+          </div>
+        </>
+    );
+}
+
+export default CommunitiesAside;
