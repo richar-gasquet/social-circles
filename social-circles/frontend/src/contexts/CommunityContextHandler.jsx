fbd77d592ee74e83538063c0441047b278a02eb6
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-13 16:53:01 -0400

    Create CommunityContextHandler to share fetch for getting different types of communities

---
+++ social-circles/frontend/src/contexts/CommunityContextHandler.jsx
@@ -0,0 +1,67 @@
+import { createContext, useContext, useState, useCallback } from "react";
+
+const CommunityContext = createContext({});
+export const useCommunityContext = () => useContext(CommunityContext);
+
+function CommunityContextProvider({ children }) {
+  const [communities, setCommunities] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [displayAlert, setDisplayAlert] = useState(null);
+
+  const fetchCommunities = useCallback(async (route) => {
+    try {
+      setIsFetching(true);
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}${route}`,
+        {
+          credentials: "include",
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setCommunities(data.results);
+      } else {
+        setDisplayAlert({
+          type: "danger",
+          header: "Could not display events!",
+          text: "Try again or contact the administrator.",
+        });
+      }
+    } catch (error) {
+      setDisplayAlert({
+        type: "danger",
+        header: "Could not connect to server!",
+        text: "Try again or contact the administrator.",
+      });
+    } finally {
+      setIsFetching(false);
+    }
+  }, []);
+
+  const updateCommunitiesOnRegistration = (group_id, registered, new_count) => {
+    setCommunities(
+      communities.map((community) => {
+        if (community.group_id === group_id) {
+          return { ...community, isRegistered: registered, count: new_count };
+        } else {
+          return community;
+        }
+      })
+    );
+  };
+
+  return (
+    <CommunityContext.Provider value={{
+        communities,
+        isFetching,
+        displayAlert,
+        setDisplayAlert,
+        fetchCommunities,
+        updateCommunitiesOnRegistration,
+    }}>
+      {children}
+    </CommunityContext.Provider>
+  );
+}
+
+export default CommunityContextProvider;
