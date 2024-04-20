40e45c395674c1747fca177012a8a76a25103c92
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-20 02:12:01 -0400

    Add "Not Available" for past events

--- social-circles/frontend/src/components/user-functions/RegisterButton.jsx
+++ social-circles/frontend/src/components/user-functions/RegisterButton.jsx
@@ -1,7 +1,9 @@
 import Button from "react-bootstrap/Button";
 
 function RegisterButton(props) {
-  const buttonText = props.isRegistered ? "Cancel Registration" : "Register"
+  const buttonText = props.isRegistered
+    ? "Cancel Registration"
+    : (props.isDisabled ? "Not available" : "Register")
   const handleOnClick = props.isRegistered ? props.handleCancel : props.handleRegister;
   return (
     <Button variant = {props.isRegistered ? "danger" : "success"}
