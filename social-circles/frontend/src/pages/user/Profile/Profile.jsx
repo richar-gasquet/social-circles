c5accbc7a318b751df2a5b81b14249b2943b82a6
Ajimelec Gonzalez <ag5935@princeton.edu>
2024-04-30 22:32:21 -0400

    Modularize Profile page code

--- social-circles/frontend/src/pages/user/Profile/Profile.jsx
+++ social-circles/frontend/src/pages/user/Profile/Profile.jsx
@@ -1,245 +1,26 @@
-import { useState, useEffect } from 'react';
-import { useUserContext } from '../../../contexts/UserContextHandler';
+import React from 'react';
+import { useFormLogic } from './ProfileFormLogic';
+import { ConfirmModal, WelcomeModal } from './ProfileModals';
 import AdminHeader from '../../../components/headers/AdminHeader';
 import UserHeader from '../../../components/headers/UserHeader';
 import GuestHeader from '../../../components/headers/GuestHeader';
 import Loading from '../../../components/shared-components/LoadingSpinner';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import styles from '../../../css/ProfileForm.module.css';
 import SessionTimeoutHandler from '../../../components/session-checker/SessionTimeoutHandler';
 import logo from "../../../assets/social-circles-logo.png";
+import styles from '../../../css/ProfileForm.module.css';
 
 function Profile() {
-  const {userData, isLoading} = useUserContext();
-  const [editMode, setEditMode] = useState(false);
-  const [formErrors, setFormErrors] = useState({});
-  const [confirmModal, setConfirmModal] = useState({ show: false, onConfirm: () => {} });
-  const [welcomeModal, setWelcomeModal] = useState(false);
-  const [formData, setFormData] = useState({
-    first_name: '',
-    last_name: '',
-    email: '',
-    address: '',
-    preferred_name: '',
-    pronouns: 'Select',
-    phone_number: '',
-    marital_status: 'Select',
-    family_circumstance: '',
-    community_status: '',
-    interests: '',
-    personal_identity: '',
-  });
+  const {
+    formData, setFormData, formErrors, isLoading, editMode, handleBlur, handleChange,
+    handleSubmit, handleCancel, handleDelete, toggleEdit, confirmModal, showConfirmModal,
+    handleDynamicSelectChange, handleConfirm, userData, welcomeModal
+  } = useFormLogic();
 
-  useEffect(() => {
-    if (userData) {
-      // Populate form data if user data exists
-      setFormData(prevFormData => ({ ...prevFormData, ...userData }));
-    }
-  }, [userData]);
-
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: type === 'checkbox' ? checked : value,
-    }));
-    
-    // Reset error for that field
-    if (formErrors[name]) {
-      setFormErrors(prevErrors => ({ ...prevErrors, [name]: '' }));
-    }
-  };
-  
-
-  const handleBlur = (e) => {
-    const { name, value } = e.target;
-    // Include validation for both regular and "Other" fields
-    if ((['first_name', 'last_name', 'address', 'phone_number', 'pronouns', 'marital_status'].includes(name) ||
-        name.endsWith('Other')) && !value.trim()) {
-      setFormErrors(prevErrors => ({ ...prevErrors, [name]: 'Please complete this field.' }));
-    }
-  };
-
-  const handleCancel = () => {
-    if (userData.is_admin === undefined) {
-      window.location.href = `${import.meta.env.VITE_BACKEND_URL}/logout`; 
-    } else {
-      // Reset form data to the initial user data and conditionally set 'Other' fields
-      setFormData(userData);
-      setEditMode(false);
-      }
-  };
-
-  const toggleEdit = () => {
-    setEditMode(true);
-    if (userData) {
-      // Populate form data if user data exists
-      const updatedFormData = { ...userData };
-
-      // Check for pronouns
-      const pronounsOptions = ['He/His', 'She/Her', 'They/Them'];
-      if (!pronounsOptions.includes(userData.pronouns)) {
-        updatedFormData.pronouns = 'Other';
-        updatedFormData.pronounsOther = userData.pronouns;
-      }
-
-      // Check for marital status
-      const maritalStatusOptions = ['Single', 'Married', 'Divorced', 'Widowed'];
-      if (!maritalStatusOptions.includes(userData.marital_status)) {
-        updatedFormData.marital_status = 'Other';
-        updatedFormData.marital_statusOther = userData.marital_status;
-      }
-      setFormData(updatedFormData);
-   }
-  };
-
-  const handleLogout = (e) => {
-    window.location.href = `${import.meta.env.VITE_BACKEND_URL}/logout`;
-  };
-
-  const updateUserData = (newData) => {
-    setUserContext(prev => ({...prev, ...newData}));
-};
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Character limits definition
-    const maxChar = {
-      first_name: 50,
-      last_name: 50,
-      address: 50,
-      phone_number: 15,
-      email: 50,
-      preferred_name: 50,
-      pronouns: 50,
-      marital_status: 50,
-      family_circumstance: 50,
-      community_status: 50,
-      interests: 50,
-      personal_identity: 50,
-    };
-
-    // Handle validation and character limit
-    const newErrors = {};
-    let characterLimitExceeded = false;
-
-    Object.keys(formData).forEach(field => {
-      if (field.endsWith('Other') && formData[field] === '' && formData[field.replace('Other', '')] === 'Other') {
-        newErrors[field] = 'Please complete this field.';
-      } else if (['first_name', 'last_name', 'address', 'phone_number'].includes(field) && !formData[field].trim()) {
-        newErrors[field] = 'Please complete this field.';
-      }
-      // Check for character limits
-      if (maxChar[field] && formData[field] && formData[field].length > maxChar[field]) {
-        newErrors[field] = `Character limit of ${maxChar[field]} exceeded.`;
-      }
-    });
-
-    if (Object.keys(newErrors).length > 0) {
-      setFormErrors(newErrors);
-      return;
-    }
-
-    // Adjust formData for submission
-    const submissionData = { ...formData };
-    ['pronouns', 'marital_status'].forEach(field => {
-      if (submissionData[field] === 'Other' && submissionData[`${field}Other`] !== undefined) {
-        submissionData[field] = submissionData[`${field}Other`];
-      }
-    });
-    // API call to update user data
-    const endpoint = userData.is_admin === undefined ? '/add-user' : '/update-user';
-    try {
-      const request = await fetch(`${import.meta.env.VITE_BACKEND_URL}${endpoint}`, {
-        credentials: "include",
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(submissionData),
-      });
-      if (request.ok) {
-        setEditMode(false);
-        if(endpoint === '/add-user') {
-          setWelcomeModal(true);
-          userData.is_admin = false;
-          updateUserData(submissionData)
-        }
-        else{
-          window.location.reload();
-        }
-      } else {
-        console.error('Failed to submit form request');
-      }
-    } catch (error) {
-      console.error('Failed to submit form:', error);
-    }
-  };
-
-  const handleDelete = () => {
-    showConfirmModal("Are you sure you want to delete your account? This action cannot be undone.", async () => {
-      try {
-        const request = await fetch(`${import.meta.env.VITE_BACKEND_URL}/delete-user`, {
-          credentials: "include",
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email: userData.email })
-        });
-        if (request.ok) {
-          console.log('User deleted successfully');
-          window.location.href = `${import.meta.env.VITE_BACKEND_URL}/logout`; 
-        } else {
-          console.error('Failed to delete user');
-        }
-      } catch (error) {
-        console.error('Error deleting user:', error);
-      }
-    });
-  };
-
-  const handleDynamicSelectChange = (e) => {
-    const { name, value } = e.target;
-    if (value === "Other") {
-      setFormData(prevFormData => ({
-        ...prevFormData,
-        [name]: value,
-        [`${name}Other`]: ''  // Initialize the "Other" field to ensure it's treated as empty for validation
-      }));
-    } else {
-      setFormData(prevFormData => ({
-        ...prevFormData,
-        [name]: value,
-        [`${name}Other`]: undefined  // Properly handle other values
-      }));
-    }
-  };
-  
-  // Function to show confirm modal
-  const showConfirmModal = (message, onConfirm) => {
-    setConfirmModal({ show: true, message, onConfirm });
-  };
-
-  // Function to handle confirm actions
-  const handleConfirm = () => {
-    confirmModal.onConfirm();
-    setConfirmModal({ ...confirmModal, show: false }); // Hide modal after confirming
-  };
-
-  const redirectToEvents = () => {
-    window.location.href = '/events'; // Modify as needed to match your routing setup
-  };
-
-
+  const Header = formData.is_admin ? AdminHeader : (formData.is_admin === false ? UserHeader : GuestHeader);
 
   const pronounsOptions = ['He/His', 'She/Her', 'They/Them', 'Other'];
   const maritalStatusOptions = ['Single', 'Married', 'Divorced', 'Widowed', 'Other'];
 
-  // Determine which header to use based on whether is_admin is undefined
-  const Header = userData.is_admin ? AdminHeader : (userData.is_admin === false ? UserHeader : GuestHeader);
 
   if (isLoading) {
     return (
@@ -248,6 +29,13 @@ function Profile() {
       </>
     )
   }
+  const handleLogout = (e) => {
+    window.location.href = `${import.meta.env.VITE_BACKEND_URL}/logout`;
+  };
+
+  const redirectToEvents = () => {
+    window.location.href = '/events'; 
+  };
 
   if (!userData || userData.email === undefined || userData.is_admin === undefined || editMode === true) {
     return (
@@ -399,36 +187,8 @@ function Profile() {
             </div>
           </div>
         </div>
-        <Modal show={confirmModal.show} onHide={() => setConfirmModal({ ...confirmModal, show: false })}>
-          <Modal.Header closeButton>
-            <Modal.Title>Confirm Action</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>{confirmModal.message}</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => setConfirmModal({ ...confirmModal, show: false })}>
-              Close
-            </Button>
-            <Button variant="danger" onClick={handleConfirm}>
-              Delete Account
-            </Button>
-          </Modal.Footer>
-        </Modal>
-        <Modal 
-          show={welcomeModal} 
-          onHide={() => {
-            setWelcomeModal(false); 
-            window.location.reload();  
-          }}> 
-          <Modal.Header closeButton>
-            <Modal.Title>Welcome!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Thank you for signing up, get started with your first event</Modal.Body>
-          <Modal.Footer>
-            <Button variant="danger" onClick={redirectToEvents}>
-              To Events
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <ConfirmModal {...confirmModal} onHide={() => setConfirmModal({ ...confirmModal, show: false })} />
+        <WelcomeModal show={welcomeModal} onHide={() => setWelcomeModal(false)} onRedirect={redirectToEvents} />
       </>
     );
   }
