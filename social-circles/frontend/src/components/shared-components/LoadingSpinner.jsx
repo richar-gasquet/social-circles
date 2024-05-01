86b709811bf0445f1afd4496de2aa8924b9bde5e
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-04-30 22:17:12 -0400

    Prettify

--- social-circles/frontend/src/components/shared-components/LoadingSpinner.jsx
+++ social-circles/frontend/src/components/shared-components/LoadingSpinner.jsx
@@ -1,14 +1,20 @@
 function Loading() {
-    return (
-        <>
-        <div className="col-12 d-flex justify-content-center" style={{paddingTop: '20em'}}>
-        <div className="spinner-border mt-5" role="status"
-            style={{ width: '10rem', height: '10rem'}}>
-            <span className="sr-only">Loading...</span>
+  return (
+    <>
+      <div
+        className="col-12 d-flex justify-content-center"
+        style={{ paddingTop: "20em" }}
+      >
+        <div
+          className="spinner-border mt-5"
+          role="status"
+          style={{ width: "10rem", height: "10rem" }}
+        >
+          <span className="sr-only">Loading...</span>
         </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Loading;
