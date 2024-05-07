c47189f01230d96783038b400910f6ff1fa5b2ad
vh8154 <144638698+vh8154@users.noreply.github.com>
2024-05-07 01:58:17 -0400

    adding contact info

--- social-circles/frontend/src/pages/shared/Contact.jsx
+++ social-circles/frontend/src/pages/shared/Contact.jsx
@@ -10,13 +10,16 @@ function Contact() {
       <GuestHeader />
       <div className="container" >
         <div className="row justify-content-center" >
-          <div className="col-md-8" style={{marginTop: '20em'}}>
+          <div className="col-md-8" style={{marginTop: '15em'}}>
             <h2 className="mb-4">Contact Social Circles</h2>
             <p>
               You can directly contact the administrator, Dana H. Moorhead at:
             </p>
-            <p>+ 1 (phone number)</p>
+            <p>+ 1 (609) 731-2847</p>
             <p>danamoorhead@gmail.com</p>
+            <br />
+            <p>For <strong>technical support</strong> please contact:</p>
+            <p>socialcircles333@gmail.com</p>
           </div>
         </div>
       </div>
