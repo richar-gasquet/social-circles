6bd41e1a65895672022080bf01d65f5d55931327
Amado Krsul Portugal <132724295+amadokrsul@users.noreply.github.com>
2024-05-07 01:45:40 -0400

    Add ability to use custom sizing and paddingTop

--- social-circles/frontend/src/components/shared-components/LoadingSpinner.jsx
+++ social-circles/frontend/src/components/shared-components/LoadingSpinner.jsx
@@ -1,15 +1,12 @@
 /* Component to display a loading spinner if necessary*/
-function Loading() {
+function Loading({ size = "10rem", paddingTop = "20em" }) {
   return (
     <>
-      <div
-        className="col-12 d-flex justify-content-center"
-        style={{ paddingTop: "20em" }}
-      >
+      <div className="col-12 d-flex justify-content-center" style={{ paddingTop }}>
         <div
-          className="spinner-border mt-5"
+          className="spinner-border"
           role="status"
-          style={{ width: "10rem", height: "10rem" }}
+          style={{ width: size, height: size }}
         >
           <span className="sr-only">Loading...</span>
         </div>
